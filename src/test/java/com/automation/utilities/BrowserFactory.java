4de1edda705b040be5c8afa28a789bb66f0f4ed3
Alok Kumar <alokkr@live.com>
2021-07-12 18:43:36 +0530

    Deleted Unused files

--- src/test/java/com/automation/utilities/BrowserFactory.java
+++ src/test/java/com/automation/utilities/BrowserFactory.java
@@ -10,23 +10,22 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 public class BrowserFactory {
 
 	public static WebDriver startApplication(WebDriver driver, String browserName, String url) {
-			if(browserName.equals("Chrome")) {
-				System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		switch (browserName) {
+			case "Chrome" -> {
+				System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver89.exe");
 				driver = new ChromeDriver();
 			}
-			else if(browserName.equals("Firefox")) {
-				System.setProperty("webdriver.gecko.driver","./Drivers/geckodriver.exe");
+			case "Firefox" -> {
+				System.setProperty("webdriver.gecko.driver", "./Drivers/geckodriver.exe");
 				driver = new FirefoxDriver();
 			}
-			else if(browserName.equals("IE")) {
+			case "IE" -> {
 				System.setProperty("webdriver.ie.driver", "./Drivers/IEDriverServer.exe");
 				driver = new InternetExplorerDriver();
 			}
-			else
-			{
-				System.out.println("Browser not supported");
-			}
-			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			default -> System.out.println("Browser not supported");
+		}
+			driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
 			driver.manage().window().maximize();
 			driver.get(url);
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
